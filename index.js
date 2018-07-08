e76cb27260ca4e0bb0ff5f851b4274b67d091c3c
Mohamed El Ghamry <mohamed.elghamry97@gmail.com>
2018-07-08 19:39:48 +0200

    removed tokenfile

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const botconfig = require("./botconfig.json");
+const tokenfile = require("./tokenfile.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({ disableEveryone: true });
@@ -38,4 +39,4 @@ bot.on("message", async message => {
 
 });
 
-bot.login(botconfig.token);
+bot.login(tokenfile.token);
